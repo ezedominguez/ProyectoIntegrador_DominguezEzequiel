f88faecd817398ea3f3dc86a42e49fa16e96b496
ezedominguez <ezequieldominguez1@hotmail.com>
2022-08-23 20:56:14 -0300

    + Implementación EDUCACIÓN en BackEnd

--- ProyectoIntegrador_DominguezEzequiel/BackEnd/src/main/java/com/portfolio/egd/Controller/CExperiencia.java
+++ ProyectoIntegrador_DominguezEzequiel/BackEnd/src/main/java/com/portfolio/egd/Controller/CExperiencia.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -69,6 +70,7 @@ public class CExperiencia {
         
     }
     
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         // Validamos si existe el ID
         if(!sExperiencia.existsById(id))
